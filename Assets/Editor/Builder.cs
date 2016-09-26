199e246a854fda26aec2609dd98fe88d9871f0f8
Kenneth Jürgensen <gnurgen@gmail.com>
2016-09-26 14:02:17 +0200

    Testing if scenes are included

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -1,7 +1,34 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using UnityEditor;
+using System.IO;
+using UnityEngine;
+using System;
 
 public class Builder{
 
+	static void AndroidBuild(){
 
+		string basePath = "C:/Builds/Android";
+		string buildFolder = System.DateTime.Now.ToString ("dd-MM-yy HH.mm.ss");
+
+		try{
+
+			string[] scenes = { "Assets/Scenes/Main.unity" };
+
+			FileUtil.DeleteFileOrDirectory ("C:/Users/Kenned/AppData/LocalUnity/Editor/Editor.log");
+
+			Directory.CreateDirectory (basePath + "/" + buildFolder);
+
+			BuildPipeline.BuildPlayer (scenes, basePath + "/" + buildFolder + "/" + "buildfile.apk" , BuildTarget.Android, BuildOptions.None);
+
+			FileUtil.CopyFileOrDirectory ("C:/Users/Kenned/AppData/Local/Unity/Editor/Editor.log", basePath + "/" + buildFolder + "/log.txt");
+		} catch(UnityException e){
+			StreamWriter fil = new StreamWriter(basePath + "/" + buildFolder + "/unity_errors.txt",true);
+			fil.Write (e.Message);
+			fil.Close ();
+		} catch(Exception e){
+			StreamWriter fil = new StreamWriter(basePath + "/" + buildFolder + "/general_errors.txt",true);
+			fil.Write (e.Message);
+			fil.Close ();
+		}
+	}
 }
