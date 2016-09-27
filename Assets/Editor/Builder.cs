302a6e6a95baf25d2c13085f56f85797e4db6115
Kenneth Jürgensen <gnurgen@gmail.com>
2016-09-27 10:34:56 +0200

    Close to done with server stuff, although very messy

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using UnityEngine;
 using System;
+using System.Collections.Generic;
 
 public class Builder{
 
@@ -14,20 +15,37 @@ public class Builder{
 
 			PlayerSettings.bundleIdentifier = "com.highfiveproductions.doedetoner";
 			PlayerSettings.bundleVersion = "2.2";
-			// Change something to push again and again and again
-			string[] scenes = Directory.GetFiles("C:/workspace/Assets/Scenes/Building/");
+
+			// All of this is to find every scene in the build folder, put the StartScene
+			// as the first scene, and add the rest.
+			string[] scenes = Directory.GetFiles("C:/workspace/Assets/Scenes/Building/"); // find all the scenes for building
 
 			for(int i = 0; i < scenes.Length; i++){
-				//scenes[i] = trimPath(scenes[i]);
-				Debug.Log ("TEST PATH: " + scenes[i]);
-				Debug.Log("TEST FILE: " + extractFile(scenes[i]));
+				scenes[i] = extractFile(scenes[i]); // Remove the path up to the file, so only the file name remains
 			}
+
+			List<string> tempScenes = new List<string>(); // Make a list holding all the scenes
+			for(int i = 0; i < scenes.Length; i++){
+				tempScenes.Add(scenes[i]);
+			}
+			tempScenes.Remove("StartScene.unity"); // Remove the start scene
+			string[] buildScenes = new string[scenes.Length]; // Make the array holding all the scenes to be build
+			buildScenes[0] = "StartScene.unity"; // Set the StartScene as the first one
+			int n = 1;
+			foreach(string s in tempScenes){ // Add the rest of the scenes
+				buildScenes[n] = s;
+				n++;
+			}
+			for(int i = 0; i < buildScenes.Length; i++){
+				buildScenes[i] = "Assets/Scenes/Building/" + scenes[i]; // Add the required path to each file name
+			}
+
 
 			FileUtil.DeleteFileOrDirectory ("C:/Users/dadiu/AppData/LocalUnity/Editor/Editor.log");
 
 			Directory.CreateDirectory (basePath + "/" + buildFolder);
 
-			BuildPipeline.BuildPlayer (scenes, basePath + "/" + buildFolder + "/" + "build.apk" , BuildTarget.Android, BuildOptions.None);
+			BuildPipeline.BuildPlayer (buildScenes, basePath + "/" + buildFolder + "/" + "build.apk" , BuildTarget.Android, BuildOptions.None);
 
 			FileUtil.CopyFileOrDirectory ("C:/Users/dadiu/AppData/Local/Unity/Editor/Editor.log", basePath + "/" + buildFolder + "/log.txt");
 		} catch(UnityException e){
