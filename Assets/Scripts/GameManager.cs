60614636319b735902ee4d407493eec33b7e6c27
Daniel Svejstrup Christensen <fusion_link@hotmail.com>
2016-09-27 12:41:14 +0200

    Game Manager update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,12 +8,8 @@ public class GameManager {
     private GameObject _player;
     private GameObject _enemy;
     private GameObject _musicBox;
-    public AudioManager audioManager;
-
-    public GameManager()
-    {
-        audioManager = new AudioManager(this);
-    }
+    private AudioManager _audioManager;
+    public int musicBoxCount = 0;
 
     public static GameManager instance
     {
@@ -40,7 +36,7 @@ public class GameManager {
         get
         {
             if (_enemy == null)
-                _enemy = GameObject.FindWithTag("Enemy");
+                _enemy = GameObject.Find("Enemy");
             return _enemy;
         }
     }
@@ -50,20 +46,108 @@ public class GameManager {
         get
         {
             if (_musicBox == null)
-                _musicBox = GameObject.FindWithTag("MusicBox");
+                _musicBox = GameObject.Find("MusicBox");
             return _musicBox;
         }
     }
 
-    public delegate void EnemyAttackAction();
-    public event EnemyAttackAction OnEnemyAttack; 
+    public AudioManager audioManager
+    {
+        get
+        {
+//            if (_audioManager == null)
+//                _audioManager = Object.FindObjectOfType(typeof (AudioManager)) as AudioManager;
+            return _audioManager;
+        }
+    }
+
+    public delegate void SpawnAction();
+    public event SpawnAction OnEnemySpawn;
+    public event SpawnAction OnEnemyDespawn;
+    public void EnemySpawn()
+    {
+        OnEnemySpawn();
+    }
+    public void EnemyDespawn()
+    {
+        OnEnemyDespawn();
+    }
+
+    public delegate void StepAction();
+    public event StepAction OnEnemyStep;
+    public event StepAction OnPlayerStep;
+    public void EnemyStep()
+    {
+        OnEnemyStep();
+    }
+    public void PlayerStep()
+    {
+        OnPlayerStep();
+    }
+
+    public delegate void AttackAction();
+    public event AttackAction OnEnemyAttack;
     public void EnemyAttack()
     {
         OnEnemyAttack();
     }
 
-    public delegate void MusicBoxPlayAction();
-    public event MusicBoxPlayAction OnMusicBoxPlay;
+    public delegate void HitAction(int value);
+    public event HitAction OnEnemyAttackHit;
+    public void EnemyAttackHit(int i)
+    {
+        OnEnemyAttackHit(i);
+    }
+
+    public delegate void FreezeAction(int value);
+    public event FreezeAction OnPlayerFreeze;
+    public event FreezeAction OnPlayerUnfreeze;
+    public void PlayerFreeze(int i)
+    {
+        OnPlayerFreeze(i);
+    }
+    public void PlayerUnfreeze(int i)
+    {
+        OnPlayerFreeze(i);
+    }
+
+    public delegate void HealthAction(int value);
+    public event HealthAction OnPlayerTakeDamage;
+    public event HealthAction OnPlayerRecover;
+    public void PlayerTakeDamage(int i)
+    {
+        OnPlayerTakeDamage(i);
+    }
+    public void PlayerRecover(int i)
+    {
+        OnPlayerRecover(i);
+    }
+
+    public delegate void SprintAction();
+    public event SprintAction OnPlayerSprintStart;
+    public event SprintAction OnPlayerSprintStop;
+    public event SprintAction OnPlayerFatigue;
+    public void PlayerSprintStart()
+    {
+        OnPlayerSprintStart();
+    }
+    public void PlayerSprintStop()
+    {
+        OnPlayerSprintStop();
+    }
+    public void PlayerFatigue()
+    {
+        OnPlayerFatigue();
+    }
+
+    public delegate void MusicBoxAction();
+    public event MusicBoxAction OnMusicBoxPlay;
+    public event MusicBoxAction OnMusicBoxStop;
+    public event MusicBoxAction OnMusicBoxPause;
+    public event MusicBoxAction OnMusicBoxResume;
+    public event MusicBoxAction OnMusicBoxRewindStart;
+    public event MusicBoxAction OnMusicBoxRewindStop;
+    public event MusicBoxAction OnMusicBoxRewinded;
     public void MusicBoxPlay()
     {
         OnMusicBoxPlay();
