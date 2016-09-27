f54e5491d4f114660362bae3ffd2cc2f56504363
Daniel Svejstrup Christensen <fusion_link@hotmail.com>
2016-09-27 08:34:10 +0200

    Game manager init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager {
+
+    private static GameManager _instance;
+    public GameObject player;
+    public GameObject enemy;
+    public GameObject musicBox;
+
+    public static GameManager instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new GameManager();
+            return _instance;
+        }
+    }
+}
