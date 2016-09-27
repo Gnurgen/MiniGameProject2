9d4e14ca24fda8f8d0cf11e4dfb443a774070371
Daniel Svejstrup Christensen <fusion_link@hotmail.com>
2016-09-27 09:44:58 +0200

    Game Manager update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,9 +4,10 @@ using System.Collections;
 public class GameManager {
 
     private static GameManager _instance;
-    public GameObject player;
-    public GameObject enemy;
-    public GameObject musicBox;
+
+    private GameObject _player;
+    private GameObject _enemy;
+    private GameObject _musicBox;
 
     public static GameManager instance
     {
@@ -17,4 +18,49 @@ public class GameManager {
             return _instance;
         }
     }
+
+    public GameObject player
+    {
+        get
+        {
+            if (_player == null)
+                _player = GameObject.FindWithTag("Player");
+            return _player;
+        }
+    }
+
+    public GameObject enemy
+    {
+        get
+        {
+            if (_enemy == null)
+                _enemy = GameObject.FindWithTag("Enemy");
+            return _enemy;
+        }
+    }
+
+    public GameObject musicBox
+    {
+        get
+        {
+            if (_musicBox == null)
+                _musicBox = GameObject.FindWithTag("MusicBox");
+            return _musicBox;
+        }
+    }
+
+    public delegate void EnemyAttackAction();
+    public event EnemyAttackAction OnEnemyAttack; 
+    public void EnemyAttack()
+    {
+        OnEnemyAttack();
+    }
+
+    public delegate void MusicBoxPlayAction();
+    public event MusicBoxPlayAction OnMusicBoxPlay;
+    public void MusicBoxPlay()
+    {
+        OnMusicBoxPlay();
+    }
+
 }
