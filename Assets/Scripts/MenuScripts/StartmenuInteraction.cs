4505f437f4aebcecc7a628c2acf2304cf5b6b0a4
Nicolai Skovhus <nskovh11@student.aau.dk>
2016-09-27 10:20:08 +0200

    Dummy startmenu. With DUMMIES (Need to be replaced) for left/right ear visual/audio.

--- Assets/Scripts/MenuScripts/StartmenuInteraction.cs
+++ Assets/Scripts/MenuScripts/StartmenuInteraction.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Net;
 
 public class StartmenuInteraction : MonoBehaviour {
 
-    AudioSource AS;
+    private AudioSource AS;
+    public Sprite earImgL, earImgR;
+   public GameObject earFB;
 
     void Start()
     {
+        earFB = GameObject.Find("EarFeedback");
+        earFB.SetActive(false);
+        earImgL = Resources.Load<Sprite>("StartMenu/leftEar");
+        earImgR = Resources.Load<Sprite>("StartMenu/rightEar");
         AS = GetComponent<AudioSource>();
+        AS.clip = Resources.Load<AudioClip>("StartMenu/leftRightAudio");
+        print("NOTE: Left/Right audio and Left/Right images are dummies - REPLACE WITH REAL OBJECTS IN RESOURCES FOLDER");
     }
 
     public void StartGameButton()
@@ -27,17 +36,27 @@ public class StartmenuInteraction : MonoBehaviour {
         StartCoroutine(stereoTest());
     }
 
-    void OnGUI()
+    void Update()
     {
 
     }
 
     private IEnumerator stereoTest()
     {
+        earFB.SetActive(true);
         AS.panStereo = -1; //Play to left ear
+        earFB.GetComponent<Image>().sprite = earImgL;
         AS.Play();
         yield return new WaitForSeconds(AS.clip.length+0.1f);
         AS.panStereo = 1; //Play to right ear
+        earFB.GetComponent<Image>().sprite = earImgR;
         AS.Play();
+        Invoke("turnImgOff", AS.clip.length + 0.1f);
+    }
+
+
+    void turnImgOff()
+    {
+        earFB.SetActive(false);
     }
 }
