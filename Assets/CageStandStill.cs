846a1bad223edcd168419bdc3dabd00c36a3c10d
Nicolai Skovhus <nskovh11@student.aau.dk>
2016-09-28 11:39:44 +0200

    +Cage tag +MonsterTestScene ##+Standstill/idlemove ##+Chaseplayer -MonsterAI script GameHandler events needs debuggin for nullrefs on events.

---
+++ Assets/CageStandStill.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CageStandStill : MonoBehaviour {
+    public bool StandStill;
+
+    void OnEnable()
+    {
+        if (GameObject.FindGameObjectWithTag("Cage") != null)
+            GameObject.FindGameObjectWithTag("Cage").SetActive(false);
+        GameManager.instance.EnemySpawn();
+        GameManager.instance.enemy.GetComponent<NavMeshController>().setCage(transform);
+        gameObject.tag = "Cage";
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject == GameManager.instance.player)
+            GameManager.instance.EnemyAggro();
+    }
+    void OnDisable()
+    {
+        GameManager.instance.EnemyDespawn();
+        gameObject.tag = "Untagged";
+    }
+}
--- Assets/Scripts/NavMeshController.cs
+++ Assets/Scripts/NavMeshController.cs
@@ -2,46 +2,78 @@
 using System.Collections;
 
 public class NavMeshController : MonoBehaviour {
-	NavMeshAgent agent;
-	[SerializeField]
-	Transform cage;
-	enum state {idle, reset, chase};
+	private NavMeshAgent agent;
+	private Transform cage;
+	private enum state {idle, chase};
 	private state curState;
-	BoxCollider bCol;
-	SphereCollider sCol;
-	Vector3 idleTar;
-	void Start () {
-		agent = GetComponent<NavMeshAgent> ();
-		bCol = cage.GetChild (0).GetComponent<BoxCollider> ();
-		sCol = cage.GetChild (0).GetComponent<SphereCollider> ();
+	private BoxCollider bCol;
+	private SphereCollider sCol;
+	private Vector3 idleTar, _dir;
+    private bool _noMove;
+
+    [SerializeField]
+    private float _idleSpeed, _chaseSpeed, _chaseTime;
+
+    void Start () {
 		curState = state.idle;
-		idleTar = getRndIdle ();
-		agent.destination = idleTar;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Vector3.Distance (transform.position, idleTar) < 0.2f) {
-			idleTar = getRndIdle ();
-			agent.destination = idleTar;
-		}
+        GameManager.instance.OnEnemyAggro += startChase;
 	}
 
-	void setCage(Transform newCage){
+    void Update() {
+        switch (curState)
+        { 
+            case state.idle:
+                if (Vector3.Distance(transform.position,idleTar) < 0.3f && !_noMove)
+                {
+                    idleTar = getRndIdle();
+                    agent.destination = idleTar;
+                }
+                break;
+            case state.chase:
+                agent.destination = GameManager.instance.player.transform.position;
+                break;
+        }
+	}
+
+	public void setCage(Transform newCage){
+        if (agent == null)
+            agent = GetComponent<NavMeshAgent>();
 		cage = newCage;
-		bCol = cage.GetChild (0).GetComponent<BoxCollider> ();
-		sCol = cage.GetChild (0).GetComponent<SphereCollider> ();
-		transform.position = cage.GetChild (0).position;
+		bCol = cage.GetChild (0).GetComponent<BoxCollider>();
+		sCol = cage.GetChild (0).GetComponent<SphereCollider>();
+        transform.position = new Vector3(cage.GetChild(0).position.x, transform.position.y, cage.GetChild(0).position.z);
+        idleTar = transform.position;
 		agent.destination = idleTar;
+        agent.speed = _idleSpeed;
+        _noMove = cage.GetComponent<CageStandStill>().StandStill;
 		curState = state.idle;
 	}
 
-	Vector3 getRndIdle(){
+	private Vector3 getRndIdle(){
 		if (bCol != null) {
-			return new Vector3 (Random.Range (cage.position.x - bCol.size.x / 2, cage.position.x + bCol.size.x / 2), transform.position.y, Random.Range (cage.position.z - bCol.size.z / 2, cage.position.z + bCol.size.z / 2));
+			return new Vector3 (Random.Range (cage.position.x - bCol.size.x / 2, cage.position.x + bCol.size.x / 2), 
+                                              transform.position.y, Random.Range (cage.position.z - bCol.size.z / 2, cage.position.z + bCol.size.z / 2));
 		} else {
 			Vector3 temp = Random.insideUnitSphere * sCol.radius + cage.GetChild (0).position;
 			return new Vector3(temp.x,transform.position.y, temp.z);
 		}
 	}
+
+    private void startChase()
+    {
+        curState = state.chase;
+        agent.speed = _chaseSpeed;
+    }
+    private void startIdle()
+    {
+        curState = state.idle;
+        agent.speed = _idleSpeed;
+        idleTar = new Vector3(cage.GetChild(0).position.x, transform.position.y, cage.GetChild(0).position.z);
+        agent.destination = idleTar;
+    }
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.transform == cage)
+            Invoke("startIdle", _chaseTime);
+    }
 }
