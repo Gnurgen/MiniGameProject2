7545f3e94e1a65a96652bda23ea78b158ffabb3e
Daniel Svejstrup Christensen <fusion_link@hotmail.com>
2016-09-29 10:06:57 +0200

    Music Box spawn list fixed

--- Assets/Scripts/MusicBox/MusicBoxSpawnList.cs
+++ Assets/Scripts/MusicBox/MusicBoxSpawnList.cs
@@ -1,7 +1,4 @@
 ﻿using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -13,46 +10,24 @@ public class MusicBoxSpawnList : ScriptableObject {
     [SerializeField]
     public List<string> list = new List<string>();
 
-    public void Add(GameObject obj)
+    public bool Add(GameObject obj)
     {
         if (!list.Contains(obj.name))
         {
             list.Add(obj.name);
-            apply();
+            return true;
         }
+        return false;
     }
 
-    private void removeInvalidEntries()
-    {
-        bool b = false;
-        for (int i = 0; i < list.Count;)
-            if (!GameObject.Find(list[i]))
-            {
-                Debug.LogError("GameObject \"" + list[i] + "\" in Music Box spawn list not found. Removing invalid entry.");
-                list.RemoveAt(i);
-                b = true;
-            }
-            else
-                i++;
-        if (b)
-            apply();
-    }
-
-    private void apply()
-    {
-#if UNITY_EDITOR
-        EditorUtility.SetDirty(this);
-        AssetDatabase.SaveAssets();
-#endif
-    }
-
-    public void Remove(GameObject obj)
+    public bool Remove(GameObject obj)
     {
         if (list.Contains(obj.name))
         {
             list.Remove(obj.name);
-            apply();
+            return true;
         }
+        return false;
     }
 
     public bool Contains(GameObject obj)
@@ -65,34 +40,33 @@ public class MusicBoxSpawnList : ScriptableObject {
         return list.IndexOf(obj.name);
     }
 
-    public void MoveUp(GameObject obj)
+    public bool MoveUp(GameObject obj)
     {
-        removeInvalidEntries();
         int index = list.IndexOf(obj.name);
         if (index + 1 < list.Count)
         {
             string temp = list[index + 1];
             list[index + 1] = list[index];
             list[index] = temp;
-            apply();
+            return true;
         }
+        return false;
     }
 
-    public void MoveDown(GameObject obj)
+    public bool MoveDown(GameObject obj)
     {
-        removeInvalidEntries();
         int index = list.IndexOf(obj.name);
         if (index - 1 >= 0)
         {
             string temp = list[index - 1];
             list[index - 1] = list[index];
             list[index] = temp;
-            apply();
+            return true;
         }
+        return false;
     }
 
     public GameObject First() {
-        removeInvalidEntries();
         if (list.Count > 0)
             return GameObject.Find(list[0]);
         return null;
@@ -100,29 +74,36 @@ public class MusicBoxSpawnList : ScriptableObject {
 
     public GameObject Next(GameObject obj)
     {
-        removeInvalidEntries();
         int index = list.IndexOf(obj.name);
         return GameObject.Find(list[index + 1 < list.Count ? index+1 : 0]);
     }
 
     public GameObject Prev(GameObject obj)
     {
-        removeInvalidEntries();
         int index = list.IndexOf(obj.name);
         return GameObject.Find(list[index - 1 >= 0 ? index-1 : list.Count-1]);
     }
 
-    public void SelectAll()
+    public GameObject[] GetAll()
     {
-#if UNITY_EDITOR
-        removeInvalidEntries();
         GameObject[] objects = new GameObject[list.Count];
         for (int i = 0; i < list.Count; i++)
-        {
             objects[i] = GameObject.Find(list[i]);
-        }
-        Selection.objects = objects;
-#endif
+        return objects;
     }
 
+    public bool RemoveInvalidEntries()
+    {
+        bool b = false;
+        for (int i = 0; i < list.Count;)
+            if (!GameObject.Find(list[i]))
+            {
+                Debug.LogError("GameObject \"" + list[i] + "\" in Music Box spawn list not found. Removing invalid entry.");
+                list.RemoveAt(i);
+                b = true;
+            }
+            else
+                i++;
+        return b;
+    }
 }
