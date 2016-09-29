d9de0bce3a443204889bb499d8e59081aea09b00
Mette <mette9jakobsen@homail.com>
2016-09-29 11:15:11 +0200

    Player can gain and loose health.

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour {
+
+    public GameObject player;
+    public float enemyAttackDist = 1.0f;
+    public float timeBetweenAttack = 5.0f;
+    public float chanceforSecondAttackHit = 0.5f;
+
+    private float currentAttackTime;
+    private float playerEnemyDistance;
+    private Rigidbody playerBody;
+    private bool playerHit = false;
+
+    // Use this for initialization
+    void Start () {
+        currentAttackTime = 0;
+        playerBody = player.GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        playerEnemyDistance = Vector3.Distance(playerBody.position, transform.position);
+
+        if (playerEnemyDistance <= enemyAttackDist)
+        {
+            Debug.Log("Enemy near!");
+            if (currentAttackTime <= 0)
+            {
+                GameManager.instance.EnemyAttack();
+                rollIfHit();
+                currentAttackTime = timeBetweenAttack;
+            }
+            else
+            {
+                currentAttackTime -= Time.deltaTime;
+                Debug.Log("Preparing attack");
+            }
+        }
+        else { }
+        Debug.Log("Health: " + currentHealth);
+    }
+
+    private void rollIfHit()
+    {
+        if (playerHit == false)
+        {
+            Debug.Log("First Hit!");
+            GameManager.instance.EnemyAttackHit(1);
+            LoseLife(1);
+            playerHit = true;
+        }
+        else
+        {
+            if (Random.value <= chanceforSecondAttackHit)
+            {
+                Debug.Log("Second Hit!");
+                GameManager.instance.EnemyAttackHit(1);
+                LoseLife(1);
+                playerHit = false;
+            }
+            else
+            {
+                playerHit = false;
+                Debug.Log("Miss!");
+            }
+        }
+    }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,44 +4,98 @@ using System.Collections;
 public class PlayerHealth : MonoBehaviour {
     
     public int health = 3;
-    public float regenTime = 10.0f;
+    public float regenTime = 5.0f;
     public int amountHealthRegenerated = 1;
+    public GameObject enemy;
+    public float enemyAttackDist = 1.0f;
+    public float timeBetweenAttack = 5.0f;
+    public float chanceforSecondAttackHit = 0.5f;
 
     private float currentHealth;
-    private float currentTime;
-
+    private float currentregenTime;
+    private float currentAttackTime;
+    private float playerEnemyDistance;
+    private Rigidbody enemyBody;
+    private bool playerHit = false;
     void Start () {
-        currentHealth = 1;
-        currentTime = regenTime;
+
+        currentHealth = health;
+        currentregenTime = regenTime;
+        currentAttackTime = 0;
         GameManager.instance.OnPlayerTakeDamage += LoseLife;
+        enemyBody = enemy.GetComponent<Rigidbody>();
     }
     void Update() {
         regenerate();
-        Debug.Log(currentHealth);
+        playerEnemyDistance = Vector3.Distance(enemyBody.position, transform.position);
+
+        if (playerEnemyDistance <= enemyAttackDist) {
+            Debug.Log("Enemy near!");
+            if (currentAttackTime <= 0)
+            {
+                GameManager.instance.EnemyAttack();
+                rollIfHit();
+                currentAttackTime = timeBetweenAttack;
+            }
+            else
+            {
+                currentAttackTime -= Time.deltaTime;
+                Debug.Log("Preparing attack");
+            }
+        }
+        else{ }
+        Debug.Log("Health: "+ currentHealth);
             
     }
 
-    void regenerate() {
-        if (currentTime >= 0)
-            currentTime -= Time.deltaTime;
+    private void rollIfHit() {
+        if (playerHit == false)
+        {
+            Debug.Log("First Hit!");
+            GameManager.instance.EnemyAttackHit(1);
+            LoseLife(1);
+            playerHit = true;
+        }
+        else {
+            if (Random.value <= chanceforSecondAttackHit)
+            {
+                Debug.Log("Second Hit!");
+                GameManager.instance.EnemyAttackHit(1);
+                LoseLife(1);
+                playerHit = false;
+            }
+            else
+            {
+                playerHit = false;
+                Debug.Log("Miss!");
+            }
+        }
+
+    }
+
+    private void regenerate() {
+        if (currentregenTime >= 0)
+            currentregenTime -= Time.deltaTime;
         else
         {
             if (currentHealth < health)
             {
                 GameManager.instance.PlayerRecover(amountHealthRegenerated);
                 GainLife();
-                currentTime = regenTime;
+                currentregenTime = regenTime;
             }
             else
-                currentTime = regenTime;
+                currentregenTime = regenTime;
         }
     }
-    void GainLife()
+    private void GainLife()
     {
         currentHealth+= amountHealthRegenerated;
     }
 
-    public void LoseLife(int value) {
+    private void LoseLife(int value) {
         currentHealth-=value;
     }
+
+
 }
