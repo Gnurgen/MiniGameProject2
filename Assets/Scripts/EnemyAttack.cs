72495628adaee08ba23d39f9b9b8a614567d534f
Mette <mette9jakobsen@homail.com>
2016-09-30 10:46:52 +0200

    Vibrations work

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -7,20 +7,21 @@ public class EnemyAttack : MonoBehaviour
     float enemyAttackDist = 2.0f;
     public float timeBetweenAttack = 1.0f;
     public float chanceforSecondAttackHit = 0.5f;
+    public long vibrationDuration = 10000;
 
     private float currentAttackTime;
     private float playerEnemyDistance;
     private Transform playerBody;
     private bool playerHit = false;
 
+
     // Use this for initialization
     void Start()
     {
         currentAttackTime = 0;
         playerBody = GameManager.instance.player.transform;
-
+        GameManager.instance.OnEnemyAttackHit += AttackVibrate;
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -41,6 +42,13 @@ public class EnemyAttack : MonoBehaviour
             }
         }
         else { }
+    }
+
+    public void AttackVibrate(int i)
+    {
+
+        //Handheld.Vibrate();
+        Vibrator.Vibrate(vibrationDuration);
     }
 
     private void rollIfHit()
