fbe5bf2024c1666ecc70469fd078ad901970f167
Kristoffer Lind Holm <klh12@student.aau.dk>
2016-10-03 17:08:51 +0200

    no message

--- Assets/Scripts/Ending.cs
+++ Assets/Scripts/Ending.cs
@@ -6,24 +6,29 @@ public class Ending : MonoBehaviour {
     private bool ending;
     private bool playerFacingDoor;
     private GameObject lighBulb;
-    private GameObject coffinLit;
-
+    private Transform coffinLit;
+    private Transform coffinLitTar;
     private Transform playerBody;
     public float pauseBeforeTurnOffLight = 2;
     public float doorspeed = 0.5f;
     private float cPause;
+    private float step=0;
+    public float SecondsOfAnimation;
     
     void Start () {
-        playerBody = GameManager.instance.player.transform;
+        playerBody = GameManager.instance.player.transform.GetChild(0);
         lighBulb = GameObject.Find("LightBulb");
-        coffinLit = GameObject.Find("coffinHatch_geo");
+        coffinLit = GameObject.Find("coffinHatch_geo").transform;
+        coffinLitTar = GameObject.Find("coffinHatch_geo_tar").transform;
+ 
     }
     
     void Update()
     {
-           musicbox = GameManager.instance.musicBoxCount;
+       
+        musicbox = GameManager.instance.musicBoxCount;
         
-        if (musicbox >= 5) { // Number of the last music box
+        if (musicbox >= 0) { // Number of the last music box
             ending = true;
         }
         if (ending) {
@@ -39,11 +44,15 @@ public class Ending : MonoBehaviour {
     }
 
     void closeCoffin() {
-        if (coffinLit.transform.localPosition.x <= -0.1)
+        if (step <=1)
         {
-            coffinLit.transform.eulerAngles = new Vector3(90, 180, 0);
-            coffinLit.transform.Translate(Vector3.right * Time.deltaTime*doorspeed, Space.Self);
-            coffinLit.transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
+            step += Time.deltaTime / SecondsOfAnimation;
+            coffinLit.position = Vector3.MoveTowards(coffinLit.position, coffinLitTar.position, step);
+            coffinLit.rotation = Quaternion.RotateTowards(coffinLit.rotation, coffinLitTar.rotation, step);
+           
+            //coffinLit.transform.eulerAngles = new Vector3(90, 180, 0);
+            //coffinLit.transform.Translate(Vector3.right * Time.deltaTime*doorspeed, Space.Self);
+            //coffinLit.transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
         }
         else {
             if (cPause >= pauseBeforeTurnOffLight)
