f8b465e2b57197334f9599b14674985019eabd2a
Daniel Svejstrup Christensen <fusion_link@hotmail.com>
2016-10-03 13:33:05 +0200

    Updated tools

--- Assets/Editor/ReplaceAnything.cs
+++ Assets/Editor/ReplaceAnything.cs
@@ -4,12 +4,12 @@ using System.Collections;
 
 public class ReplaceAnything : EditorWindow {
 
-    private static Object _replacement;
-    private static bool _useTranslation;
-    private static bool _useRotation;
-    private static bool _useScale;
+    private Object _replacement;
+    private bool _useTranslation = true;
+    private bool _useRotation = true;
+    private bool _useScale = true;
 
-    [MenuItem("Tools/Replace Anything/With Prefab")]
+    [MenuItem("Tools/Replace Anything",false,0)]
     public static void ShowWindow()
     {
         EditorWindow.GetWindow(typeof(ReplaceAnything));
@@ -20,35 +20,35 @@ public class ReplaceAnything : EditorWindow {
 
         _replacement = EditorGUILayout.ObjectField("Prefab:", _replacement, typeof(GameObject), false) as GameObject;
 
-        if (_replacement != null)
+        GUI.enabled = _replacement != null;
+
+        GUILayout.Space(10);
+
+        GUILayout.Label("Maintain:");
+        _useTranslation = EditorGUILayout.Toggle("Translation", _useTranslation);
+        _useRotation = EditorGUILayout.Toggle("Rotation", _useRotation);
+        _useScale = EditorGUILayout.Toggle("Scale", _useScale);
+
+        GUILayout.Space(10);
+
+        if (GUILayout.Button("Replace Selection"))
         {
-            GUILayout.Space(10);
-
-            GUILayout.Label("Maintain:");
-            _useTranslation = EditorGUILayout.Toggle("Translation", _useTranslation);
-            _useRotation = EditorGUILayout.Toggle("Rotation", _useRotation);
-            _useScale = EditorGUILayout.Toggle("Scale", _useScale);
-
-            GUILayout.Space(10);
-
-            if (GUILayout.Button("Replace Selection"))
+            Object[] objects = Selection.objects;
+            Object[] selection = new Object[objects.Length];
+            for (int i = 0; i < objects.Length; i++)
             {
-                Object[] objects = Selection.objects;
-                Object[] selection = new Object[objects.Length];
-                for (int i = 0; i < objects.Length; i++)
-                {
-                    GameObject obj = PrefabUtility.InstantiatePrefab(_replacement) as GameObject;
-                    Undo.RegisterCreatedObjectUndo(obj, "Created replacement object");
-                    applyTransform(obj, (objects[i] as GameObject).transform);
-                    Undo.DestroyObjectImmediate(objects[i]);
-                    selection[i] = obj;
-                }
-                Selection.objects = selection;
+                GameObject obj = PrefabUtility.InstantiatePrefab(_replacement) as GameObject;
+                Undo.RegisterCreatedObjectUndo(obj, "Created replacement object");
+                applyTransform(obj, (objects[i] as GameObject).transform);
+                Undo.DestroyObjectImmediate(objects[i]);
+                selection[i] = obj;
             }
+            Selection.objects = selection;
         }
+        GUI.enabled = true;
     }
 
-    private static void applyTransform(GameObject obj, Transform transform)
+    private void applyTransform(GameObject obj, Transform transform)
     {
         if (_useTranslation)
             obj.transform.position = transform.position;
